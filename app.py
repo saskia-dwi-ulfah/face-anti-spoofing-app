4eee2339c09e3334142f29f146da3828f17ed803
Saskia Dwi Ulfah <73946560+saskia-dwi-ulfah@users.noreply.github.com>
2024-01-18 15:50:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,6 @@ def streamlit_image_to_cv2(image):
     image = image[:, :, ::-1].copy()
     return image
 
-def f1_score(y_true, y_pred): #taken from old keras source code
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    recall = true_positives / (possible_positives + K.epsilon())
-    f1_score = 2*(precision*recall)/(precision+recall+K.epsilon())
-    return f1_score
-
 ## ----
 
 st.title("Simple Face Anti-Spoofing Application 🤥")
